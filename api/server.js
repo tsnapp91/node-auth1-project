76ac184780ae90b69275f9920eea2b65501ded8c
Travis Snapp <traviswsnapp@outlook.com>
2023-12-26 11:24:15 -0500

    auth and user router connected

--- api/server.js
+++ api/server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 
+const usersRouter = require("./users/users-router");
+const authRouter = require("./auth/auth-router");
+
 /**
   Do what needs to be done to support sessions with the `express-session` package!
   To respect users' privacy, do NOT send them a cookie unless they log in.
@@ -21,11 +24,15 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
+server.use("/api/users", usersRouter);
+server.use("/api/auth", authRouter);
+
 server.get("/", (req, res) => {
   res.json({ api: "up" });
 });
 
-server.use((err, req, res, next) => { // eslint-disable-line
+server.use((err, req, res, next) => {
+  // eslint-disable-line
   res.status(err.status || 500).json({
     message: err.message,
     stack: err.stack,
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,5 +1,5 @@
 // Require the `restricted` middleware from `auth-middleware.js`. You will need it here!
-
+const router = require("express").Router();
 
 /**
   [GET] /api/users
@@ -24,5 +24,10 @@
   }
  */
 
+router.get("/", (req, res, next) => {
+  res.json("user router connected");
+});
 
 // Don't forget to add the router to the `exports` object so it can be required in other modules
+
+module.exports = router;
