baf44270eab8a3ed1ee0352a38909382c6ec727d
Travis Snapp <traviswsnapp@outlook.com>
2023-12-26 14:31:37 -0500

    mvp completed

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -18,7 +18,10 @@ function findBy(filter) {
   resolves to the user { user_id, username } with the given user_id
  */
 function findById(user_id) {
-  return db("users").select("user_id", "username").where("user_id", user_id);
+  return db("users")
+    .select("user_id", "username")
+    .where("user_id", user_id)
+    .first();
 }
 
 /**
